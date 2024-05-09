f39a7e346f614e6726d58f573604164858f236dd
Liam Stark <williamsigbinoba2@gmail.com>
2024-05-09 13:59:10 +0100

    added functions

--- StringManipulation.js
+++ StringManipulation.js
@@ -9,7 +9,7 @@ var button = document.querySelector(".reverse");
 function reverseString(string) {
     var yourString = "";
 
-    for (let i = string.length - 1; i >= 0; i--){
+    for (let i = string.length - 1; i >= 0; i--) {
         yourString += string[i]
     }
 
@@ -23,15 +23,43 @@ function countStringChar(string) {
     characterCount.innerHTML = `<h2>${characterAmount}</h2>`
 }
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     reverseString(nameInput.value);
     upperCasify()
 })
 
-function upperCasify(){
+function upperCasify() {
     ourSentence.classList.add("uppercaseEm")
 }
 
+
+let numbers = [3, 7, 15, 22, 9, 12, 5, 18];
+
+// Log minimum and maximum numbers
+// Let's say One Digit number is a minimum number &
+var minimumDigit = 9;
+
+// For loop approach
+function findHigherNum() {
+    var highestNumber = numbers.filter((number) => number > minimumDigit);
+    console.log("These are the higher numbers " + highestNumber);
+    // Log the higher numbers
+
+    var lowestNumbers = numbers.filter((number) => number < minimumDigit);
+    console.log("These are the Lowest numbers " + lowestNumbers);
+    // Log the Lower Numbers
+}
+
+function sumArrValues(){
+    var arraySum = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        arraySum += numbers[index];
+    }
+    console.log(arraySum)
+}
+
+sumArrValues();
+findHigherNum();
 // function capitalizeFirstLetter(sentence) {
 //     // Split the sentence into an array of words
 //     let words = sentence.split(" ");
